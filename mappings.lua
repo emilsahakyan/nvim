8eba6f912192d1bcdf59b0aff811e4189055ce68
Emil Sahakyan <emamajanyan@gmail.com>
2022-07-29 11:42:06 +0400

    Add key mappings.

--- mappings.lua
+++ mappings.lua
@@ -5,6 +5,10 @@ return {
     ["<C-s>"] = { ":w!<cr>", desc = "Save File" },
     ["<F2>"] = {":set spell <Enter>", desc = "Set spell" },
     ["<F3>"] = {":set nospell <Enter>", desc = "No spell"},
+    ["<F4>"] = {":noh <Enter>", desc = "No highlight"},
+    ["<Tab>"]  = {"%", desc = "Matching pair"},
+    ["<leader>\""] = {"viW<esc>a\"<esc>gvo<esc>i\"<esc>gvo<esc>3l", desc = "Quote under cursor"},
+    ["<leader>\'"] = {"viW<esc>a\'<esc>gvo<esc>i\'<esc>gvo<esc>3l", desc = "Quote under cursor"},
   },
   t = {
     -- setting a mapping to false will disable it
