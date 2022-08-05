905f3b2f307452982999dc3c766e8f2fedccfb5f
Emil Mamajanyan <emil@vorak-solutions.com>
2022-08-05 16:19:05 +0400

    install LSP servers automatically

--- plugins/nvim-lsp-installer.lua
+++ plugins/nvim-lsp-installer.lua
@@ -1,4 +1,3 @@
 return {
-    ensure_installed = { },
-    --ensure_installed = { "sumneko_lua", "pyright", "clangd", "cmake", "jsonls", "verible", "html" },
+    ensure_installed = { "sumneko_lua", "pyright", "clangd", "cmake", "jsonls", "verible", "html" },
 }
