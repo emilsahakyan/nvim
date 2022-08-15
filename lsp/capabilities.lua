e89274b2a9b42301e26f971d3ff6fe641f338f26
Emil Mamajanyan <emil@vorak-solutions.com>
2022-08-15 20:57:39 +0400

    CMP to show some more text via lspkind plugin
    More capabilities..

--- lsp/capabilities.lua
+++ lsp/capabilities.lua
@@ -1,2 +1,2 @@
---local capabilities = vim.lsp.protocol.make_client_capabilities()
---return require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+return require("cmp_nvim_lsp").update_capabilities(capabilities)
